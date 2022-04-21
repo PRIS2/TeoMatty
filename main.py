97b232df8414afc64530c1e8df5de2a74d833319
TeoMatty <101330508+TeoMatty@users.noreply.github.com>
2022-04-21 13:40:55 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
-    print("Hello world!")
-  
-  
-      print("Hello world!")
-    
-    
-        print("Hello world!")
+import cmath, math, numpy
+
+def addComplex( z1, z2):
+    return z1 + z2
+
+def subComplex(z1, z2):
+  return z1-z2
+
+z1 = complex(2, 3)
+z2 = complex(1, 2)
+print( "Addition is : ", addComplex(z1, z2))
+
+z1= complex(2,3)
+z2= complex(1,2)
+print("substraction is: ", subComplex(z1,z2))
+
+z = complex(2,5)
+print(" conjugate is: ", z.conjugate())
+
+c = 4+ 4j
+# phase
+phase = cmath.phase(c)
+print('4+ 4j Phase =', phase)
+print('Phase in Degrees =', numpy.degrees(phase))
+print('-4-4j Phase =', cmath.phase(-4-4j), 'radians. Degrees =', numpy.degrees(cmath.phase(-4-4j)))
+# we can get phase using math.atan2() function too
+print('Complex number phase using math.atan2() =', math.atan2(2, 1))
