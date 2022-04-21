157b268fcfa40c54c3a1dee8c7ece58abb820ef5
TeoMatty <101330508+TeoMatty@users.noreply.github.com>
2022-04-21 13:47:00 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,16 @@
 import cmath, math, numpy
-
 def addComplex( z1, z2):
     return z1 + z2
-
 def subComplex(z1, z2):
   return z1-z2
-
 z1 = complex(2, 3)
 z2 = complex(1, 2)
 print( "Addition is : ", addComplex(z1, z2))
-
 z1= complex(2,3)
 z2= complex(1,2)
 print("substraction is: ", subComplex(z1,z2))
-
 z = complex(2,5)
 print(" conjugate is: ", z.conjugate())
-
 c = 4+ 4j
 # phase
 phase = cmath.phase(c)
