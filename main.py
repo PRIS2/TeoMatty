f61e917ad0956391b6d985c46bcbc0a4e2f17a00
TeoMatty <101330508+TeoMatty@users.noreply.github.com>
2022-04-27 16:41:13 +0200

    Update main.py

--- main.py
+++ main.py
@@ -19,3 +19,21 @@ print('Phase in Degrees =', numpy.degrees(phase))
 print('-4-4j Phase =', cmath.phase(-4-4j), 'radians. Degrees =', numpy.degrees(cmath.phase(-4-4j)))
 # we can get phase using math.atan2() function too
 print('Complex number phase using math.atan2() =', math.atan2(2, 1))
+z=3+2j
+z.real
+z=3+2j
+z.imag
+z1 = 2 + 3j
+z2 = 4 + 5j
+z1 * z2
+z1 = 2 + 3j
+z2 = 4 + 5j
+z1 / z2
+algebraic = 3 + 2j
+geometric = complex(3, 2)
+radius, angle = cmath.polar(algebraic)
+trigonometric = radius * (cmath.cos(angle) + 1j*cmath.sin(angle))
+exponential = radius * cmath.exp(1j*angle)
+
+for number in algebraic, geometric, trigonometric, exponential:
+  print(format(number, "g"))
